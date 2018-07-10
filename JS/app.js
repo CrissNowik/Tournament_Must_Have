d4b70f8868776d8475ee620f1722a0a21a2752f7
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2018-07-10 18:47:17 +0200

    JS getTeams func

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,30 @@
-    // import 'domElems' from 'elems';
-    // console.log(domElems.btnConfirm);
-
 $(document).ready(function(){
+
+    let domElems = {
+        btnGenerate: $('.collector__generate'),
+        btnConfirm: $('.nav__confirm'),
+        collector: $('.collector'),
+        naviScreen: $('.nav'),
+        result: $('.result'),
+        btnAdd: $('.collector__add'),
+        teamList: $('.collector__list'),
+        teamInput: $('.collector__input'),
+        btnDel: $('.collector__del')
+    }
 
     function switchingScreens (toHide,toShow){
         toHide.hide();
         toShow.show();
     };
+
+    function getTeams() {
+        let newTeam = domElems.teamInput.val();
+        domElems.teamList.append(`<li> ${newTeam} </li>`);
+        // TODO
+        // button in each li for removing it from list
+    };
+
+
 
     domElems.btnConfirm.on('click', function(e){
         e.preventDefault();
@@ -15,6 +33,11 @@ $(document).ready(function(){
     domElems.btnGenerate.on('click', function(e){
         e.preventDefault();
         switchingScreens(domElems.collector,domElems.result);
+    });
+
+    domElems.btnAdd.on('click', function(e){
+        e.preventDefault();
+        getTeams();
     });
 
   
--- JS/elems.js
+++ JS/elems.js
@@ -1,7 +1,2 @@
-export default domElems = {
-                    btnGenerate: $('.collector__generate'),
-                    btnConfirm: $('.nav__confirm'),
-                    collector: $('.collector'),
-                    naviScreen: $('.nav'),
-                    result: $('.result')
-                }
+
+
