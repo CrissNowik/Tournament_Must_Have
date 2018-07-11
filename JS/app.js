b4e5de133b02f50ec229e15255a120acd9af712a
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2018-07-11 23:40:17 +0200

    JS data-nr for new li, JS choosingTournamentType

--- JS/app.js
+++ JS/app.js
@@ -1,36 +1,78 @@
 $(document).ready(function(){
-
-    let domElems = {
-        btnGenerate: $('.collector__generate'),
-        btnConfirm: $('.nav__confirm'),
-        collector: $('.collector'),
-        naviScreen: $('.nav'),
-        result: $('.result'),
-        btnAdd: $('.collector__add'),
-        teamList: $('.collector__list'),
-        teamInput: $('.collector__input'),
-        teamOnList: $('collector__listItem')
-    }    
     
+    //DOM elements store
+    //
+    let domElems = {
+            btnGenerate: $('.collector__generate'),
+            btnConfirm: $('.nav__confirm'),
+            collector: $('.collector'),
+            naviScreen: $('.nav'),
+            result: $('.result'),
+            btnAdd: $('.collector__add'),
+            teamList: $('.collector__list'),
+            teamInput: $('.collector__input'),
+            teamOnList: $('collector__listItem'),
+            btnGenerate: $('.collector__generate'),
+            selectedTournamentType: $('#collector__select.children :selected').val()
+    }     
+    
+    //globalVariables store
+    // 
+    let globalVariables = {
+            dataCounter: 0
+    }
+    
+    //Functions 
+    //
     function switchingScreens (toHide,toShow){
         toHide.hide();
         toShow.show();
     };
 
-
-    // TO DO - id for each li
-    function getTeams(count) {
-        console.log(count);
-        
+    function gettingTeams() {
+        let counter = globalVariables.dataCounter ++;
         let newTeam = domElems.teamInput.val();
         domElems.teamList.append(
-            `<li class="collector__listItem" data-nr="${count.lenght}"> ${newTeam} ${count.lenght}
+            `<li class="collector__listItem" data-nr="${counter}"> ${newTeam}
                 <button class="collector__del" type="button">
                 X
                 </button> 
             </li>`);     
     };
 
+    function choosingTournamentType(tournamentType) {
+        if (tournamentType === 'League') {
+            leagueGenerator();
+        } else if (tournamentType === 'Cup') {
+            cupGenerator();
+        } else {
+            mixGenerator();
+        } 
+    }
+
+    function leagueGenerator() {
+        console.log("LIGA");
+        
+    }
+
+    function cupGenerator() {
+        console.log("CUP");
+        
+    }
+    function mixGenerator() {
+        console.log("MIX");
+        
+    }
+    
+    //Removing teams from list
+    //
+    domElems.teamList.on('click', 'li.collector__listItem>button.collector__del', function(e){
+        e.preventDefault();     
+        $(this).parent().remove();
+    });
+    
+    //Function calls on elems
+    //
     domElems.btnConfirm.on('click', function(e){
         e.preventDefault();
         switchingScreens(domElems.naviScreen,domElems.collector);
@@ -44,7 +86,7 @@ $(document).ready(function(){
         e.preventDefault();
         let code = e.keyCode || e.which;
         if (code === 13 || e.type === 'click') {
-            getTeams();
+            gettingTeams();
         };  
     });
 
@@ -52,15 +94,14 @@ $(document).ready(function(){
         let code = e.keyCode || e.which;
         if (code === 13) {
             e.preventDefault();
-            getTeams();
+            gettingTeams();
         };  
     });
-
-    domElems.teamList.on('click', 'li.collector__listItem>button.collector__del', function(e){
+    
+    domElems.btnGenerate.on('click', function(e){
         e.preventDefault();     
-        $(this).parent().remove();
-    });
-  
-
- 
+        console.log("na przycisku: ", domElems.selectedTournamentType);
+        
+        choosingTournamentType(domElems.selectedTournamentType);
+    })
 });
