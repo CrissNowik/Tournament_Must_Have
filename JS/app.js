fa7d36aeb0e6332303dcf62ec27d039afde5cc2c
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2018-07-13 18:52:09 +0200

    improve alerts on collector screen

--- JS/app.js
+++ JS/app.js
@@ -1,43 +1,50 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     //DOM elements store
     //
     let domElems = {
-            btnGenerate: $('.collector__generate'),
-            btnConfirm: $('.nav__confirm'),
-            collector: $('.collector'),
-            naviScreen: $('.nav'),
-            result: $('.result'),
-            btnAdd: $('.collector__add'),
-            teamList: $('.collector__list'),
-            teamInput: $('.collector__input'),
-            teamOnList: $('collector__listItem')
-    }                             
-    
+        btnGenerate: $('.collector__generate'),
+        btnConfirm: $('.nav__confirm'),
+        collector: $('.collector'),
+        naviScreen: $('.nav'),
+        result: $('.result'),
+        btnAdd: $('.collector__add'),
+        teamList: $('.collector__list'),
+        teamInput: $('.collector__input'),
+        teamOnList: $('.collector__listItem'),
+        collectorAlertA: $('#alertOne'),
+        collectorAlertB: $('#alertTwo')
+    }
+
     //globalVariables store
     // 
     let globalVariables = {
-            dataCounter: 0
+        dataCounter: 0
     }
-    
+
     //Functions 
     //
-    function switchingScreens (toHide,toShow){
+    function switchingVisibility(toHide, toShow) {
         toHide.hide();
         toShow.show();
     };
 
     function gettingTeams() {
         let newTeam = domElems.teamInput.val();
-        if (newTeam != "") {
-            let counter = globalVariables.dataCounter++;
-            
-            domElems.teamList.append(
-                `<li class="collector__listItem" data-nr="${counter}"> ${newTeam}<button class="collector__del" type="button">X</button></li>`);     
-        } else {
-            alert("Name a team");
+
+        if (domElems.teamList.children().length > 1){
+            domElems.collectorAlertB.hide();
         }
         
+        if (newTeam != "") {
+            let counter = globalVariables.dataCounter++;
+            switchingVisibility(domElems.collectorAlertA, domElems.teamList);
+            domElems.teamList.append(
+                `<li class="collector__listItem" data-nr="${counter}"> ${newTeam}<button class="collector__del" type="button">X</button></li>`);
+        } else {
+            domElems.collectorAlertA.show();
+        }
+
     };
 
     function choosingTournamentType(tournamentType) {
@@ -47,62 +54,64 @@ $(document).ready(function(){
             cupGenerator();
         } else {
             mixGenerator();
-        } 
+        }
     };
 
     function leagueGenerator() {
         console.log("LIGA");
-        
+
     };
 
     function cupGenerator() {
         console.log("CUP");
-        
+
     };
+
     function mixGenerator() {
         console.log("MIX");
-        
+
     };
-    
+
     //Removing teams from list
     //
-    domElems.teamList.on('click', 'li.collector__listItem>button.collector__del', function(e){
-        e.preventDefault();     
+    domElems.teamList.on('click', 'li.collector__listItem>button.collector__del', function (e) {
+        e.preventDefault();
         $(this).parent().remove();
     });
-    
+
     //Function calls on elems
     //
-    domElems.btnConfirm.on('click', function(e){
+    domElems.btnConfirm.on('click', function (e) {
         e.preventDefault();
-        switchingScreens(domElems.naviScreen,domElems.collector);
+        switchingVisibility(domElems.naviScreen, domElems.collector);
     });
 
-    domElems.btnGenerate.on('click', function(e){
+    domElems.btnGenerate.on('click', function (e) {
         e.preventDefault();
         if (domElems.teamList.children().length > 2) {
+            switchingVisibility(domElems.collectorAlertB, domElems.result);
             let selectedTeam = $('#collector__select :selected').val();
-            switchingScreens(domElems.collector,domElems.result);
+            switchingVisibility(domElems.collector, domElems.result);
             choosingTournamentType(selectedTeam);
         } else {
-            alert("You need at least 3 teams!");
+            domElems.collectorAlertB.show();
         }
-        
+
     });
 
-    domElems.btnAdd.bind('click keypress', function(e){
+    domElems.btnAdd.bind('click keypress', function (e) {
         e.preventDefault();
         let code = e.keyCode || e.which;
         if (code === 13 || e.type === 'click') {
             gettingTeams();
-        };  
+        };
     });
 
-    domElems.teamInput.on('keypress', function(e){
+    domElems.teamInput.on('keypress', function (e) {
         let code = e.keyCode || e.which;
         if (code === 13) {
             e.preventDefault();
             gettingTeams();
-        };  
+        };
     });
 });
