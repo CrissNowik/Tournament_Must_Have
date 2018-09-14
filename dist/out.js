105b1634866b33a81cc1344099801723c635ee77
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2018-09-14 22:29:19 +0200

    fast fix for ladder

--- dist/out.js
+++ dist/out.js
@@ -123,7 +123,7 @@ var globalVariables = exports.globalVariables = {
     dataCounter: 0,
     idCharArr: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"],
     empty: "  . . . . . . . . . . . . . . ",
-    lucky: "  Lucky Team"
+    lucky: " Lucky Team"
 };
 
 /***/ }),
--- js/globalVariables.js
+++ js/globalVariables.js
@@ -2,5 +2,5 @@ export let globalVariables = {
     dataCounter: 0,
     idCharArr: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"],
     empty: "  . . . . . . . . . . . . . . ",
-    lucky: "  Lucky Team"
+    lucky: " Lucky Team"
 };
